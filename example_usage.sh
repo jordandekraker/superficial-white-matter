9eb3e105f2d5442982edabb3948591973ad1649e
Jordan DeKraker <jdekrake@uwo.ca>
2023-10-31 11:51:28 -0400

    example works with BIDS & micapipe

--- example_usage.sh
+++ example_usage.sh
@@ -4,13 +4,25 @@ DERIV='/data/mica3/BIDS_MICs/derivatives'
 SUB='HC002'
 SES='01'
 
-# import some nifti/gifti data
+# format some nifti/gifti data
 mkdir test
-mri_convert $DERIV/fastsurfer/sub-${SUB}_ses-${SES}/mri/aparc+aseg.mgz test/aparc+aseg.nii.gz
-tform='' #$DERIV/micapipe_v0.2.0/sub-${SUB}/ses-${SES}/xfm/sub-${SUB}_ses-${SES}_from-fsnative_to_nativepro_T1w_0GenericAffine.mat
-antsApplyTransforms -i test/aparc+aseg.nii.gz -r $DERIV/micapipe_v0.2.0/sub-${SUB}/ses-${SES}/anat/sub-${SUB}_ses-${SES}_space-nativepro_T1w.nii.gz -o test/aparc+aseg_space-nativepro.nii.gz -n MultiLabel
-cp $DERIV/micapipe_v0.2.0/sub-${SUB}/ses-${SES}/surf/sub-${SUB}_ses-${SES}_hemi-R_space-nativepro_surf-fsLR-32k_label-white.surf.gii test/wm.surf.gii
+mri_convert \
+  $DERIV/fastsurfer/sub-${SUB}_ses-${SES}/mri/aparc+aseg.mgz \
+  $DERIV/sWM/sub-${SUB}_ses-${SES}/sub-${SUB}_ses-${SES}/_space-fsnative_aparc+aseg.nii.gz
+tform='' # $DERIV/micapipe_v0.2.0/sub-${SUB}/ses-${SES}/xfm/sub-${SUB}_ses-${SES}_from-fsnative_to_nativepro_T1w_0GenericAffine.mat
+antsApplyTransforms \
+  -i $DERIV/sWM/sub-${SUB}_ses-${SES}/sub-${SUB}_ses-${SES}/_space-fsnative_aparc+aseg.nii.gz \
+  -r $DERIV/micapipe_v0.2.0/sub-${SUB}/ses-${SES}/anat/sub-${SUB}_ses-${SES}_space-nativepro_T1w.nii.gz \
+  -o $DERIV/sWM/sub-${SUB}_ses-${SES}/sub-${SUB}_ses-${SES}/_space-nativepro_aparc+aseg.nii.gz \
+  -n MultiLabel
+#  -t $tform
+# even if the tform is not needed, this resampling is necessary to standardize nifti headers
 
 # run
-python sWM/laplace_solver.py test/aparc+aseg_space-nativepro.nii.gz test/wm-laplace.nii.gz
-python sWM/surface_generator.py test/wm.surf.gii test/wm-laplace.nii.gz test/depth
+python sWM/laplace_solver.py \
+  $DERIV/sWM/sub-${SUB}_ses-${SES}/sub-${SUB}_ses-${SES}/_space-nativepro_aparc+aseg.nii.gz \
+  $DERIV/sWM/sub-${SUB}_ses-${SES}/sub-${SUB}_ses-${SES}/_space-nativepro_laplace-wm.nii.gz 
+python sWM/surface_generator.py \
+  $DERIV/micapipe_v0.2.0/sub-${SUB}/ses-${SES}/surf/sub-${SUB}_ses-${SES}_hemi-R_space-nativepro_surf-fsLR-32k_label-white.surf.gii \
+  $DERIV/sWM/sub-${SUB}_ses-${SES}/sub-${SUB}_ses-${SES}/_space-nativepro_laplace-wm.nii.gz \
+  $DERIV/sWM/sub-${SUB}_ses-${SES}/sub-${SUB}_ses-${SES}/_space-nativepro_label-sWF_depth- 
