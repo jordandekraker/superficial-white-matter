4ad4e5d920863d517515cb8912d6ad68a851db2e
y0ungeunii <y0ungeunii.h@gmail.com>
2023-12-23 16:30:11 -0500

    cleanup the code

--- sWM/surface_generator.py
+++ sWM/surface_generator.py
@@ -12,7 +12,7 @@ NIFTI  :    str
 OUTPUT :    str
             path and name to the output surfaces
 DEPTHS :    list [int | float] (OPTIONAL)
-            DEFAULT=[1,2,3] List of depths to sample (in voxels)
+            DEFAULT=[1,2,3] List of depths to sample (in mm)
 
 Returns
 -------
@@ -71,7 +71,7 @@ depth_str = [f'{d:.1f}' for d in depth_mm]  # Use one decimal places
 
 convergence_threshold = 1e-4
 step_size = 0.1 # vox
-max_iters = int(np.max(np.diff(depth_mm))/step_size)*10
+max_iters = int(np.max(np.diff(depth_vox))/step_size)*10
 
 # laplace to gradient
 dx,dy,dz = np.gradient(lp)
